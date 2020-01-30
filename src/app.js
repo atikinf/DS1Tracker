842efb623398488788725696c430a9080eb54952
Nikita Filippov <nikitaf@attu1.cs.washington.edu>
2020-01-29 16:55:49 -0800

    changed map link

--- src/app.js
+++ src/app.js
@@ -1,6 +1,15 @@
+// Copyright 2020 Nikita Filippov
+// Nikita Filippov, 1242477, nikitaf@cs.washington.edu 
+// Use however or something, idk
+
 import * as d3 from 'd3';
 import * as satellite from 'satellite.js';
 
+/*
+ * Functions for plotting a satellite's path on the Earth
+ * NOTE: var json is passed in via php in this implementation, 
+ * so you need to define it yourself normally
+ */
 
 // Potential ToDo: Tweak width
 const width = window.innerWidth * 0.95;
@@ -51,8 +60,10 @@ const trackArr = intervals.map(function(x) {
 // Current altitude in kilometers
 const altitude = satellite.eciToGeodetic(satellite.propagate(satrec, currentDate).position).height;
 
-const earth = d3.image(
-    "https://gist.githubusercontent.com/jake-low/d519e00853b15e9cec391c3dab58e77f/raw/6e796038e4f34524059997f8e1f1c42ea289d805/ne1-small.png",
+const earth_url = "https://gist.githubusercontent.com/atikinf/b3b50057151a30884b22949f448b7185/raw/1aff4fcfd6798e69223ee5e2405961b9a9c4557f/NE1_50M_SR_W.jpg";
+const backup_url = "https://gist.githubusercontent.com/jake-low/d519e00853b15e9cec391c3dab58e77f/raw/6e796038e4f34524059997f8e1f1c42ea289d805/ne1-small.png";
+
+const earth = d3.image(earth_url,
     {crossOrigin: "anonymous"})
     .then(function(img) {
         context.lineWidth = 1 + width / 2000;
